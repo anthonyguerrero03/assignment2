9bd904364c2f3807590abf1448a009fe9653bfa3
Anthony_Guerrero <aguerr12@charlotte.edu>
2024-09-25 21:26:19 -0400

    feat: implement check_resources method in SandwichMaker

--- sandwhich_maker.py
+++ sandwhich_maker.py
@@ -2,3 +2,11 @@
 class SandwichMaker:
     def __init__(self, resources):
         self.machine_resources = resources
+
+    def check_resources(self, ingredients):
+        """Returns True when order can be made, False if ingredients are insufficient."""
+        for item in ingredients:
+            if ingredients[item] > self.machine_resources[item]:
+                print(f"Sorry, there is not enough {item}.")
+                return False
+        return True
